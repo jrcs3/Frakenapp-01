e6bf162e0865626fe7690f2bd605b4de249f1316
Jack Stephens <jack@jrcs3.com>
2022-02-12 09:25:26 -0800

    Refactor workspace to have 3 parts; remake VSCode Launch so that all 3 parts are debuggable

--- typescript-apps/phone-home-electron/renderer.ts
+++ typescript-apps/phone-home-electron/renderer.ts
@@ -1,22 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Inter Process Communication module for the renderer process
-    const ipcRenderer = require('electron').ipcRenderer;
 
-    // Recieves data from the main process   
-    ipcRenderer.on('received-data', function (evt, message) {
-        const newElement: HTMLElement = document.createElement('p');
-        newElement.textContent = message;
-        document.getElementById("received-data-content").appendChild(newElement);
-    });
+async function main() {
+    /*const isDevelopment = require('electron-is-dev');
+
+    if (isDevelopment) {
+        // this is to give Chrome Debugger time to attach to the new window 
+        await new Promise(r => setTimeout(r, 1000));
+    }
+
+    debugger;
+    */
     
-    // Sends data back to the main process'ms
-    // when the phoneHome button is clicked
-    document.getElementById('phoneHome').addEventListener('click', function (event) {
-        const messagecontrol: HTMLInputElement = document.getElementById('messageText') as HTMLInputElement;
-        const messageValue: String = messagecontrol.value;
-        if (messageValue){
-            ipcRenderer.send('phoneHome', messageValue);
-            messagecontrol.value = "";
-        }
+    document.addEventListener('DOMContentLoaded', function () {
+        // Inter Process Communication module for the renderer process
+        const ipcRenderer = require('electron').ipcRenderer;
+
+        // Recieves data from the main process   
+        ipcRenderer.on('received-data', function (evt, message) {
+            const newElement: HTMLElement = document.createElement('p');
+            newElement.textContent = message;
+            document.getElementById("received-data-content").appendChild(newElement);
+        });
+
+        // Sends data back to the main process'ms
+        // when the phoneHome button is clicked
+        document.getElementById('phoneHome').addEventListener('click', function (event) {
+            const messagecontrol: HTMLInputElement = document.getElementById('messageText') as HTMLInputElement;
+            const messageValue: String = messagecontrol.value;
+            if (messageValue) {
+                ipcRenderer.send('phoneHome', messageValue);
+                messagecontrol.value = "";
+            }
+        });
     });
-});
+}
+
+main();
