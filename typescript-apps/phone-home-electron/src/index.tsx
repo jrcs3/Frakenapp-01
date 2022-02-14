28c53c280816f9a9b157fbcb6e4b5bcbe07cabb3
Jack Stephens <jack@jrcs3.com>
2022-02-13 19:52:11 -0800

    React appears to be running

---
+++ typescript-apps/phone-home-electron/src/index.tsx
@@ -0,0 +1,34 @@
+import { App } from './components/app.js';
+import { render } from 'react-dom';
+import * as React from 'react';
+/*
+async function main() {
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        // Inter Process Communication module for the renderer process
+        const ipcRenderer = require('electron').ipcRenderer;
+
+        // Recieves data from the main process   
+        ipcRenderer.on('received-data', function (evt, message) {
+            const newElement: HTMLElement = document.createElement('p');
+            newElement.textContent = message;
+            document.getElementById("received-data-content").appendChild(newElement);
+        });
+
+        // Sends data back to the main process'ms
+        // when the phoneHome button is clicked
+        document.getElementById('phoneHome').addEventListener('click', function (event) {
+            const messagecontrol: HTMLInputElement = document.getElementById('messageText') as HTMLInputElement;
+            const messageValue: String = messagecontrol.value;
+            if (messageValue) {
+                ipcRenderer.send('phoneHome', messageValue);
+                messagecontrol.value = "";
+            }
+        });
+    });
+}
+
+main();
+*/
+
+render(<App />, document.getElementById('root'));
